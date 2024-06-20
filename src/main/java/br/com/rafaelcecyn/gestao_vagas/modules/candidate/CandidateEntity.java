2af785b2efc46819f94883278a0728f870064ac5
Rafael Cecyn <rafael.mendes112@hotmail.com>
2024-06-20 20:17:00 -0300

    Criando estrutura do projeto

---
+++ src/main/java/br/com/rafaelcecyn/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,19 @@
+package br.com.rafaelcecyn.gestao_vagas.modules.candidate;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+
+}
