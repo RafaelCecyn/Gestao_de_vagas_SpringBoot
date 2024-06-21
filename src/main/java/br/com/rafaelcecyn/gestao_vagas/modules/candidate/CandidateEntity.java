dc25d96ca046f02230fa7a741132ea6786875f31
Rafael Cecyn <rafael.mendes112@hotmail.com>
2024-06-20 21:08:03 -0300

    Inserindo validacao

--- src/main/java/br/com/rafaelcecyn/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/rafaelcecyn/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,7 +1,10 @@
 package br.com.rafaelcecyn.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -10,8 +13,12 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaços")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
+    @Length(min = 10, max = 100, message = "A senha deve ter entre 10 e 100 caracteres")
     private String password;
     private String description;
     private String curriculum;
