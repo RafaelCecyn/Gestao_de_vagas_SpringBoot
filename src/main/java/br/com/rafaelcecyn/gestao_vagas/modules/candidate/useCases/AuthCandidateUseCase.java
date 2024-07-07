2622daef30aecad28e3659119ee9b7b5e60e4888
Rafael Cecyn <rafael.mendes112@hotmail.com>
2024-07-07 10:12:12 -0300

    Configurando PreAuthorize

--- src/main/java/br/com/rafaelcecyn/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
+++ src/main/java/br/com/rafaelcecyn/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -46,7 +46,7 @@ public class AuthCandidateUseCase {
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
         var expires_in = Instant.now().plus(Duration.ofMinutes(10));
         var token = JWT.create().withIssuer("javagas").withSubject(candidate.getId().toString())
-                .withClaim("roles", Arrays.asList("candidate"))
+                .withClaim("roles", Arrays.asList("CANDIDATE"))
                 .withExpiresAt(expires_in)
                 .sign(algorithm);
 
--- src/main/java/br/com/rafaelcecyn/gestao_vagas/modules/security/SecurityCandidateFilter.java
+++ src/main/java/br/com/rafaelcecyn/gestao_vagas/modules/security/SecurityCandidateFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -23,7 +25,7 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        SecurityContextHolder.getContext().setAuthentication(null);
+        //SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
         if(request.getRequestURI().startsWith("/candidate")) {
@@ -35,8 +37,11 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
                     return;
                 }
                 request.setAttribute("candidate_id",token.getSubject());
-                System.out.println("==========TOKEN==");
-                System.out.println(token.getClaim("roles"));
+                var roles = token.getClaim("roles").asList(Object.class);
+                var grants = roles.stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase())).toList();
+
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(token.getSubject(),null,grants);
+                SecurityContextHolder.getContext().setAuthentication(auth);
             }
         }
         filterChain.doFilter(request,response);
